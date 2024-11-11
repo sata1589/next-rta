b57e06c1b1e4c6a00e6099200c6f0c556d1bc14f
sata <monoceller@gmail.com>
2024-11-11 12:41:09 +0900

    fix

--- src/app/thread/[id]/content/page.tsx
+++ src/app/thread/[id]/content/page.tsx
@@ -1,8 +1,12 @@
 import { getAllCommentByThread } from "@/actions/comment";
 import { CommentList } from "@/components/thread/commentList";
 
-export default async function Thread({ params }: { params: { id: string } }) {
-  const { id } = await params;
+export default async function Thread({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const id = (await params).id;
   const allComments = await getAllCommentByThread(id);
   return (
     <main>
